5c1f8c1269e8092ae1045f25c5e957011191918c
Carl Holwell <carl.holwell@gmail.com>
2025-04-21 18:45:49 +0100

    result type

---
+++ src/main/kotlin/Result.kt
@@ -0,0 +1,9 @@
+package cholwell.dev.result
+
+sealed interface Result<out V, out E> {
+    data class Ok<out V>(val value: V): Result<V, Nothing>
+    data class Error<out E>(val error: E): Result<Nothing, E>
+}
+
+fun <V> ok(value: V) = Result.Ok(value)
+fun <E> error(error: E) = Result.Error(error)
